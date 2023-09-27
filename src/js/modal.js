6d38297444eab565156df586983b78714f1515f4
[Diogo Dias] <[diasdiogo429@gmail.com]>
2023-09-27 18:28:50 -0300

    adicionado modal e sua funcionalidade abrir e fechar

---
+++ src/js/modal.js
@@ -0,0 +1,155 @@
+var abrirModalAC = document.getElementById("BR-AC" )
+var abrirModalAL = document.getElementById("BR-AL" )
+var abrirModalAM = document.getElementById("BR-AM" )
+var abrirModalAP = document.getElementById("BR-AP" )
+var abrirModalBA = document.getElementById("BR-BA" )
+var abrirModalCE= document.getElementById("BR-CE" )
+var abrirModalDF= document.getElementById("BR-DF" )
+var abrirModalES= document.getElementById("BR-ES" )
+var abrirModalGO= document.getElementById("BR-GO" )
+var abrirModalMA= document.getElementById("BR-MA" )
+var abrirModalMG= document.getElementById("BR-MG" )
+var abrirModalMS= document.getElementById("BR-MS" )
+var abrirModalMT= document.getElementById("BR-MT" )
+var abrirModalPA= document.getElementById("BR-PA" )
+var abrirModalPB = document.getElementById("BR-PE" )
+var abrirModalPE = document.getElementById("BR-PB" )
+var abrirModalPI = document.getElementById("BR-PI" )
+var abrirModalPR = document.getElementById("BR-PR" )
+var abrirModalRJ = document.getElementById("BR-RJ" )
+var abrirModalRN = document.getElementById("BR-RN" )
+var abrirModalRO = document.getElementById("BR-RO" )
+var abrirModalRR = document.getElementById("BR-RR" )
+var abrirModalRS = document.getElementById("BR-RS" )
+var abrirModalSC = document.getElementById("BR-SC" )
+var abrirModalSE = document.getElementById("BR-SE" )
+var abrirModalSP = document.getElementById("BR-SP" )
+var abrirModalTO = document.getElementById("BR-TO" )
+
+
+
+var fecharModal = document.getElementById("fechar-modal")
+var modal = document.getElementById("meu-modal")
+
+window.addEventListener("click", (e) =>{
+    if (e.target == modal){
+        modal.style.display = "none";
+    }
+})
+
+
+
+
+
+
+fecharModal.addEventListener("click", () =>{
+    modal.style.display = "none"
+})
+
+abrirModalAC.addEventListener("click", () => {
+    modal.style.display = "flex"
+})
+
+abrirModalAL.addEventListener("click", () => {
+    modal.style.display = "flex"
+})
+
+abrirModalAM.addEventListener("click", () => {
+    modal.style.display = "flex"
+})
+
+abrirModalAP.addEventListener("click", () => {
+    modal.style.display = "flex"
+})
+
+abrirModalBA.addEventListener("click", () => {
+    modal.style.display = "flex"
+})
+
+abrirModalCE.addEventListener("click", () => {
+    modal.style.display = "flex"
+})
+
+abrirModalDF.addEventListener("click", () => {
+    modal.style.display = "flex"
+})
+
+abrirModalES.addEventListener("click", () => {
+    modal.style.display = "flex"
+})
+
+abrirModalGO.addEventListener("click", () => {
+    modal.style.display = "flex"
+})
+
+abrirModalMA.addEventListener("click", () => {
+    modal.style.display = "flex"
+})
+
+abrirModalMG.addEventListener("click", () => {
+    modal.style.display = "flex"
+})
+
+abrirModalMS.addEventListener("click", () => {
+    modal.style.display = "flex"
+})
+
+abrirModalMT.addEventListener("click", () => {
+    modal.style.display = "flex"
+})
+
+abrirModalPA.addEventListener("click", () => {
+    modal.style.display = "flex"
+})
+
+abrirModalPE.addEventListener("click", () => {
+    modal.style.display = "flex"
+})
+
+abrirModalPB.addEventListener("click", () => {
+    modal.style.display = "flex"
+})
+
+abrirModalPI.addEventListener("click", () => {
+    modal.style.display = "flex"
+})
+
+abrirModalPR.addEventListener("click", () => {
+    modal.style.display = "flex"
+})
+
+abrirModalRJ.addEventListener("click", () => {
+    modal.style.display = "flex"
+})
+
+abrirModalRN.addEventListener("click", () => {
+    modal.style.display = "flex"
+})
+
+abrirModalRO.addEventListener("click", () => {
+    modal.style.display = "flex"
+})
+
+abrirModalRR.addEventListener("click", () => {
+    modal.style.display = "flex"
+})
+
+abrirModalRS.addEventListener("click", () => {
+    modal.style.display = "flex"
+})
+
+abrirModalSC.addEventListener("click", () => {
+    modal.style.display = "flex"
+})
+
+abrirModalSE.addEventListener("click", () => {
+    modal.style.display = "flex"
+})
+
+abrirModalSE.addEventListener("click", () => {
+    modal.style.display = "flex"
+})
+
+abrirModalTO.addEventListener("click", () => {
+    modal.style.display = "flex"
+})
